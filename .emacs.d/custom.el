0f7327d180a78327792c7bf74074d69760ac4163
Stewart Perry <stewart.c.perry@gmail.com>
2023-08-19 10:42:42 +0100

    Fix x86 gnu assembly comment char

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -19,6 +19,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(align-to-tab-stop nil)
+ '(asm-comment-char ?#)
  '(column-number-mode t)
  '(css-indent-offset 4)
  '(custom-enabled-themes '(gruvbox-dark-hard))
